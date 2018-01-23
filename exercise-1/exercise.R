2158654fc6486dd262f5ebaa4a831679a1ff2e73
Clement Mooc <theclementm@hotmail.com>
2018-01-22 17:13:06 -0800

    finalize

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -5,20 +5,31 @@
 ## http://www.footballdb.com/teams/nfl/seattle-seahawks/results
 ## here 'Result' displays scores, 'L' stands for loss, 'W' for win
 
+  first_five_points = c(9,12,27,46,16)
+
 ## Create a vector of the number of points the Seahawks have allowed to be scored against them in the
 ## first 5 games
 
+  first_five_given = c(17,9,33,18,10)
 
 ## Combine your two vectors into a dataframe
 
+  points = data.frame(first_five_points, first_five_given)
 
 ## Create a new column "diff" that is the difference in points
 
-
+  diff = abs(first_five_points - first_five_given)
+  points$difference = diff
+  
 ## Create a new column "won" which is TRUE if the Seahawks won
 
+  won = first_five_points > first_five_given
+  points$won = won
 
 ## Create a vector of the opponents name (such as "Atlanta Falcons")
 
+  opponent = c("Packers", "SF 49ers", "Titans", "Colts", "Rams")
 
 ## Add the vector of opponents into the data frame
+
+  points$opponent = opponent
